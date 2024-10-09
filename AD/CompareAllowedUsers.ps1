0e1fc30805ca14abf5ed0e914a5c6c68684710cf
ExitEject <150757931+ExitEject@users.noreply.github.com>
2024-10-09 16:15:09 -0700

    Update CompareAllowedUsers.ps1

--- AD/CompareAllowedUsers.ps1
+++ AD/CompareAllowedUsers.ps1
@@ -33,9 +33,52 @@ yellow
 black
 "@ -split "`n"
 
+# Function to disable unauthorized accounts
+function Disable-UnauthorizedAccounts {
+    param (
+        [string[]]$unauthorized_users
+    )
+    
+    foreach ($user in $unauthorized_users) {
+        # Disable the account (uncomment the following line to actually disable)
+        # Disable-LocalUser -Name $user
+        Write-Host "Disabling account: $user"
+    }
+}
+
+# Function to check if accounts are disabled
+function Check-AccountsDisabled {
+    param (
+        [string[]]$unauthorized_users
+    )
+
+    foreach ($user in $unauthorized_users) {
+        $account = Get-LocalUser -Name $user -ErrorAction SilentlyContinue
+        if ($account -and -not $account.Enabled) {
+            Write-Host "Account $user is disabled."
+        } else {
+            Write-Host "Account $user is not disabled or does not exist."
+        }
+    }
+}
+
 # Find unauthorized users
 $unauthorized_users = $current_users | Where-Object { $_ -notin $allowed_users }
 
 # Print unauthorized users
 Write-Host "Unauthorized users:"
 $unauthorized_users
+
+# Ask the user if they want to disable the accounts
+if ($unauthorized_users.Count -gt 0) {
+    $response = Read-Host "Do you want to disable the unauthorized accounts? (yes/no)"
+    if ($response -eq 'yes') {
+        Disable-UnauthorizedAccounts -unauthorized_users $unauthorized_users
+        # Check if the accounts are disabled
+        Check-AccountsDisabled -unauthorized_users $unauthorized_users
+    } else {
+        Write-Host "No accounts will be disabled."
+    }
+} else {
+    Write-Host "No unauthorized users found."
+}
